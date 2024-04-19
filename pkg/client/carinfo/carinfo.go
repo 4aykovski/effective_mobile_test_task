ac9873093475af7476e978229dd45ccd6aaba2d2
4aykovski <kan89kan@gmail.com>
2024-04-19 09:23:49 +0300

    feat: create carinfo structure with method to get car info by regNumber from third-party api

---
+++ pkg/client/carinfo/carinfo.go
@@ -0,0 +1,41 @@
+package carinfo
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/4aykovski/effective_mobile_test_task/pkg/client"
+)
+
+const (
+	getCarInfoByRegNumberMethod = "/info"
+)
+
+type Client struct {
+	httpClient *client.HTTPClient
+}
+
+func NewClient(httpClient *client.HTTPClient) *Client {
+	return &Client{
+		httpClient: httpClient,
+	}
+}
+
+func (c *Client) GetCarInfoByRegNumber(regNumber string) ([]byte, error) {
+	u := c.httpClient.GetUlrWithMethods(getCarInfoByRegNumberMethod)
+	q := url.Values{}
+	q.Add("regNum", regNumber)
+
+	req, err := c.httpClient.CreateRequest(http.MethodGet, u.String(), nil, nil, q)
+	if err != nil {
+		return nil, fmt.Errorf("can't get car info: %w", err)
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("can't get car info: %w", err)
+	}
+
+	return res, nil
+}
