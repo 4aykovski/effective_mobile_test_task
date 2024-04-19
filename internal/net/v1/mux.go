98fbb0e19c7a90f8356ff8a3606cbf2143ae2628
4aykovski <kan89kan@gmail.com>
2024-04-19 14:40:23 +0300

    feat: create mux initialization

---
+++ internal/net/v1/mux.go
@@ -0,0 +1,49 @@
+package v1
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/4aykovski/effective_mobile_test_task/internal/net/v1/handler"
+	"github.com/4aykovski/effective_mobile_test_task/internal/net/v1/middleware"
+	"github.com/4aykovski/effective_mobile_test_task/internal/service/carservice"
+	"github.com/4aykovski/effective_mobile_test_task/internal/service/ownerservice"
+	"github.com/4aykovski/effective_mobile_test_task/pkg/client/carinfo"
+	"github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
+)
+
+type carService interface {
+	AddNewCar(ctx context.Context, car carservice.AddNewCarInput) error
+}
+
+type ownerService interface {
+	AddNewOwner(ctx context.Context, owner ownerservice.AddNewOwnerInput) error
+}
+
+type carInfoService interface {
+	GetCarInfoByRegNumber(ctx context.Context, regNumber string) (*carinfo.CarInfo, error)
+}
+
+func NewMux(
+	log *slog.Logger,
+	carService carService,
+	ownerService ownerService,
+	carInfoService carInfoService,
+) *chi.Mux {
+	var (
+		carHandler = handler.NewCarHandler(carInfoService, carService, ownerService)
+		mux        = chi.NewMux()
+	)
+
+	mux.Use(chiMiddleware.RequestID)
+	mux.Use(middleware.Logger(log))
+
+	mux.Route("/api/v1", func(r chi.Router) {
+		r.Route("/cars", func(r chi.Router) {
+			r.Post("/", carHandler.AddNewCar(log))
+		})
+	})
+
+	return mux
+}
