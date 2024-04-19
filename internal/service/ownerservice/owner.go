4fa4d73212d6bb1d007e66504c2655131f20a154
4aykovski <kan89kan@gmail.com>
2024-04-19 13:44:58 +0300

    feat: add owner service

---
+++ internal/service/ownerservice/owner.go
@@ -0,0 +1,43 @@
+package ownerservice
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/4aykovski/effective_mobile_test_task/internal/model"
+)
+
+type ownerRepository interface {
+	AddNewOwner(ctx context.Context, owner *model.Owner) error
+}
+
+type Service struct {
+	ownerRepository ownerRepository
+}
+
+func New(ownerRepository ownerRepository) *Service {
+	return &Service{
+		ownerRepository: ownerRepository,
+	}
+}
+
+type AddNewOwnerInput struct {
+	Name       string
+	Surname    string
+	Patronymic string
+}
+
+func (s *Service) AddNewOwner(ctx context.Context, input AddNewOwnerInput) error {
+	owner := &model.Owner{
+		Name:       input.Name,
+		Surname:    input.Surname,
+		Patronymic: input.Patronymic,
+	}
+
+	err := s.ownerRepository.AddNewOwner(ctx, owner)
+	if err != nil {
+		return fmt.Errorf("can't add new owner: %w", err)
+	}
+
+	return nil
+}
